26bb1a4f5bcfe4ea891e48273f6c0bf759c8c7cb
Petar1304 <kovacevic1304@gmail.com>
2023-07-09 15:32:02 +0200

    added colors to terminal

--- src/menu.py
+++ src/menu.py
@@ -1,19 +1,23 @@
 from calibration import calibrate
 from analysis import useMouse
+import colorama
+from colorama import Fore, Back
+
+colorama.init(autoreset=True)
 
 while True:
-    print('''
+    print(Fore.GREEN + '''
     Select option:
     1) calibration
     2) use mouse
     3) quit
     ''')
-    command = input('Enter Command\n>> ')
+    command = input(Fore.BLUE + 'Command:\n>> ')
     if command == '1':
         calibrate()
     elif command == '2':
         useMouse()
     elif command == '3': break
     else:
-        print('Wrong command')
+        print(Fore.RED + 'Wrong command')
 
